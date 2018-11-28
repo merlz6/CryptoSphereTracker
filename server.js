3a0391864ed5e1eaa977617cbaf745bac50d9932
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-11-28 15:41:46 -0500

    server running

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config()
+const session = require('express-session');
+
+
+
+
+// ******* PORT *******
+const PORT = process.env.PORT || 3000;
+
+
+// =======================================
+//              GLOBAL CONFIG
+// =======================================
+const db = mongoose.connection;
+
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/'+ 'cryptoTracker';
+
+
+
+// Connect to Mongo
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+db.once('open', ()=>{
+  console.log('Connected to Mongo');
+})
+
+// =======================================
+//             ERROR/SUCCESS MESSAGES
+// =======================================
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// =======================================
+//              MIDDLEWARE
+// =======================================
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// =======================================
+//              STATIC
+// =======================================
+app.use(express.static('public'));
+
+// =======================================
+//              CONTROLLERS
+// =======================================
+
+
+
+
+
+
+// =======================================
+//            ROUTES
+// =======================================
+
+app.get('/', (req, res)=>{
+  res.send('Hello world');
+});
+
+
+
+
+
+
+
+// =======================================
+//            LISTENER
+// =======================================
+app.listen(PORT, () => {
+  console.log('Cars Store app listening on port: '+PORT)
+});
